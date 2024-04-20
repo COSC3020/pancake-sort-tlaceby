4f8662b91070ed69a881716d2b963ab0fe181e05
tlaceby <tylerbadgertutorials@gmail.com>
2024-04-20 12:02:39 -0600

    Added code and gitignore

--- code.js
+++ code.js
@@ -1,8 +1,42 @@
+/**
+ * @param {number[]} array
+ * @param {number} n
+ * @returns {number[]}
+ */
 function flip(array, n) {
-    return array;
+  if (n > array.length) {
+    n = array.length;
+  }
+
+  // [...revseredSection, ...restOfArray]
+  return [...array.slice(0, n).reverse(), ...array.slice(n)];
 }
 
 // Use only flip() here to manipulate the array
+/**
+ * @param {number[]} array
+ * @returns {number[]}
+ */
 function pancakeSort(array) {
-    return array;
+  const n = array.length;
+
+  for (let i = n; i > 1; i--) {
+    let maxIndex = 0;
+
+    for (let j = 1; j < i; j++) {
+      if (array[j] > array[maxIndex]) {
+        maxIndex = j;
+      }
+    }
+
+    if (maxIndex !== i - 1) {
+      if (maxIndex !== 0) {
+        array = flip(array, maxIndex + 1);
+      }
+
+      array = flip(array, i);
+    }
+  }
+
+  return array;
 }
